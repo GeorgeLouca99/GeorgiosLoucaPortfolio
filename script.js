dc189e1ca0bbb911fba307aa55d3e6216766ce05
GeorgiosLouca <gl674@uowmail.edu.au>
2024-08-01 17:39:31 +1000

    2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a')
+let navLinks = document.querySelector('header nav a');
+
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -23,3 +24,4 @@ menuIcon.onClick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
