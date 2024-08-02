b321eb615b294b643a3f4837b06040825182de85
GeorgiosLouca <gl674@uowmail.edu.au>
2024-08-02 13:19:26 +1000

    22222

--- send_email.php
+++ send_email.php
@@ -2,13 +2,15 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
     $to = "georgelouca755@gmail.com";
-    $subject = "New message from $name";
+    $subject = "New message from $name: $subject";
     $headers = "From: $email";
 
-    $mailBody = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $mailBody = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
 
     if (mail($to, $subject, $mailBody, $headers)) {
         echo "Message sent successfully!";
@@ -17,3 +19,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
